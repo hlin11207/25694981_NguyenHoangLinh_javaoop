78f21a90238d938a2618f4263ffb701d75c30890
linh nguyen <hoanglinh1582007@gmail.com>
2026-09-20 21:13:46 +0700

    Add HinhChuNhatMain.java file

--- src/main/java/java/tuan01/bai01/HinhChuNhatMain.java
+++ src/main/java/java/tuan01/bai01/HinhChuNhatMain.java
@@ -1,6 +1,6 @@
 package java.tuan01.bai01;
 
-public class Main {
+public class HinhChuNhatMain {
     public static void main(String[] args) {
         HinhChuNhat h1 = new HinhChuNhat(3, 6);
         System.out.println("Chieu dai hinh chu nhat: " + h1.getChieuDai());
