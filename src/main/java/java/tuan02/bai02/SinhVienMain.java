2cb7f4de611a8b35ff83d67598720dadb94dc04d
linh nguyen <hoanglinh1582007@gmail.com>
2026-09-20 21:14:25 +0700

    Add SinhVienMain.java file

--- src/main/java/java/tuan02/bai02/SinhVienMain.java
+++ src/main/java/java/tuan02/bai02/SinhVienMain.java
@@ -2,7 +2,7 @@ package java.tuan02.bai02;
 
 import java.util.Scanner;
 
-public class Main {
+public class SinhVienMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
