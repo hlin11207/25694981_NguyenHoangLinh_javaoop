5d949956967a50d10c7f3a984cbe7357afb0eae2
linh nguyen <hoanglinh1582007@gmail.com>
2026-09-20 21:12:04 +0700

    Rename SinhVienDemo class to Main

--- src/main/java/java/tuan02/bai02/Main.java
+++ src/main/java/java/tuan02/bai02/Main.java
@@ -2,7 +2,7 @@ package java.tuan02.bai02;
 
 import java.util.Scanner;
 
-public class SinhVienDemo {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
